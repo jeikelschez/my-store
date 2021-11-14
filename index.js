9f69e9a161af600c051d58c98a69ab963aa0f635
Jeikel Sanchez <jeikelschez@hotmail.com>
2021-11-14 17:08:23 -0400

    Cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/nueva-ruta", (req, res) => {
   res.send("Hola, soy una nueva ruta");
